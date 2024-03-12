ff13bd9e892d5924a3b1f9044ed7f28a77c0a9dd
andrei <andrei@mail.com>
2024-03-12 22:18:34 +0300

    remove print

--- app/utils.py
+++ app/utils.py
@@ -5,12 +5,10 @@ def hash(password: str):
     bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
-    print(hash)
     return hash
 
 
 def verify_password(password: str, hashed_password: str):
     userBytes = password.encode('utf-8')
     result = bcrypt.checkpw(userBytes, hashed_password)
-    print(result)
     return result
